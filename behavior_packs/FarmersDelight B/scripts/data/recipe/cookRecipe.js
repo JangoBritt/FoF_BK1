47cce726174254e4bda3f0c565a3c393e784ad94
Adam Davidson <adam@jangobritt.com>
2025-03-28 16:55:39 +0000

    Farmers' Delight Update 3.2.0

--- behavior_packs/FarmersDelight B/scripts/data/recipe/cookRecipe.js
+++ behavior_packs/FarmersDelight B/scripts/data/recipe/cookRecipe.js
@@ -11,10 +11,16 @@ const vanillaItemList = [
     "minecraft:potato",
     "minecraft:rabbit",
     "minecraft:salmon",
+    //Fables
     "nicothekid:rat_raw",
+    //Expansive Biomes
     "korbon:crocodile_meat_raw",
     "korbon:frog_leg_raw",
-    "korbon:venison_raw"
+    "korbon:venison_raw",
+    //better_on_bedrock
+    "better_on_bedrock:beef_patty_raw",
+    "better_on_bedrock:raw_deer_meat",
+    "better_on_bedrock:raw_mutton_chops"
 ];
 export { vanillaItemList };
 //# sourceMappingURL=cookRecipe.js.map
