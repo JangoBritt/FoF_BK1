a72997d3e714450b96cc5cd15630de71f9aea9f0
Adam Davidson <adam@jangobritt.com>
2025-05-16 08:24:12 +0100

    FD Knife - new grasses

--- behavior_packs/FarmersDelight B/scripts/item/Knife.js
+++ behavior_packs/FarmersDelight B/scripts/item/Knife.js
@@ -96,17 +96,24 @@ export class Knife {
             if (!container)
                 return;
             ItemUtil.damageItem(container, player.selectedSlotIndex);
-            if (blockTypeId == "minecraft:tallgrass") {
+            if (blockTypeId == "minecraft:tallgrass" || blockTypeId == "minecraft:tall_dry_grass") {
+                spawnLoot('farmersdelight/straw_from_tallgrass', block.dimension, block.location);
+            }
+            //Fables
+            else if (blockTypeId == "minecraft:short_grass" || blockTypeId == "minecraft:fern" || blockTypeId == "minecraft:short_dry_grass" || blockTypeId == "korbon:dune_grass") {
                 spawnLoot('farmersdelight/straw_from_grass', block.dimension, block.location);
             }
-            else if (blockTypeId == "minecraft:short_grass" || blockTypeId == "minecraft:fern") {
-                spawnLoot('farmersdelight/straw_from_grass', block.dimension, block.location);
-            }
-            else if (blockTypeId == "minecraft:wheat") {
+            else if (blockTypeId == "minecraft:wheat")  {
                 const age = permutation.getState('growth');
                 if (age == 7)
                     spawnLoot('farmersdelight/straw', block.dimension, block.location);
             }
+
+            //Fables
+            else if (blockTypeId == "korbon:wild_wheat") {
+                spawnLoot('farmersdelight/straw', block.dimension, block.location);
+            }
+
             else if (blockTypeId == "farmersdelight:rice_block_upper") {
                 const age = permutation.getState('farmersdelight:growth');
                 if (age == 3)
