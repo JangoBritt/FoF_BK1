f5f52dcd5a2b9640b033ed0a5f3896bbb5ee9485
Adam Davidson <adam@jangobritt.com>
2025-05-29 14:52:01 +0100

    Ravager Milk

--- behavior_packs/Medal_Pack/scripts/main.js
+++ behavior_packs/Medal_Pack/scripts/main.js
@@ -1,2 +1,10 @@
 import { world, system } from "@minecraft/server";
-import "./double_slabs.js"
+import "./double_slabs.js";
+import "./ravager_milk.js";
+import "./utils.js";
+import { FoFMilkMug } from "./milk_mug_block.js";
+
+
+world.beforeEvents.worldInitialize.subscribe((e) => {
+    e.blockComponentRegistry.registerCustomComponent("fables_misc:ravager_milk_mug", new FoFMilkMug());
+})
